a3a556605e5b3b2ea8c897cfde614f0a3e999e38
MoskvichevViktor <beldetal@yandex.ru>
2021-04-29 23:15:29 +0300

    J3-5 finish

---
+++ src/main/java/race/Car.java
@@ -0,0 +1,63 @@
+package race;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+
+public class Car implements Runnable {
+    private static int CARS_COUNT;
+    private static boolean winner;
+
+    static {
+        CARS_COUNT = 0;
+    }
+
+    private Race race;
+    private int speed;
+    private String name;
+    private int count;
+    private CyclicBarrier cb;
+    private CountDownLatch cdl;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+      public Car(Race race, int speed, CyclicBarrier cb, CountDownLatch cdl) {
+        this.race = race;
+        this.speed = speed;
+        CARS_COUNT++;
+        this.name = "Участник #" + CARS_COUNT;
+        this.cb = cb;
+        this.cdl = cdl;
+    }
+
+    @Override
+    public void run() {
+        try {
+            System.out.println(this.name + " готовится");
+            Thread.sleep(500 + (int) (Math.random() * 800));
+            System.out.println(this.name + " готов");
+            cb.await();
+            cb.await();
+            for (int i = 0; i < race.getStages().size(); i++) {
+                race.getStages().get(i).go(this);
+            }
+            checkWin(this);
+            cb.await();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static synchronized void checkWin(Car car) {
+        if (!winner) {
+            System.out.println(car.name + " - WIN");
+            winner = true;
+        }
+    }
+}
+
---
+++ src/main/java/race/Stage.java
@@ -0,0 +1,7 @@
+package race;
+
+public abstract class Stage {
+    protected int length;
+    protected String description;
+    public abstract void go(Car c);
+}
